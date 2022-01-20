eca6be04cf48116f8beb5a8ba67daacd8c3eba9c
David Williamson <dwanthony02@gmail.com>
2022-01-20 14:46:01 -0700

    add davids page, rename old index

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const port = 8083
+const app = express();
+const port = 8083;
 
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
 
-app.use(bodyParser())
-app.use(express.static(__dirname + "/client"))
-app.get("/", function (req, res) {
-  console.log("Returning Index")
-  res.sendFile(path.join(__dirname, "client", "index.html"))
-})
+app.use(bodyParser());
+app.use(express.static(__dirname + "/client"));
+app.get("/tire-cart-1", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "tire-cart-1.html"));
+});
+app.get("/tire-cart-2", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "tire-cart-2.html"));
+});
 
 app.get("/registration", function (req, res, next) {
-  console.log("you got me!")
-  res.status(200).json({})
-})
+  res.status(200).json({});
+});
 
 app.post("/registration", function (req, res, next) {
-  console.log("you posted to me!")
-  console.log(req.body)
-  res.status(201).json({ message: "you posted" })
-})
+  console.log(req.body);
+  res.status(201).json({ message: "you posted" });
+});
 
-app.listen(port)
-console.log(`Listening on ${port}`)
+app.listen(port);
+console.log(`Listening on ${port}`);
